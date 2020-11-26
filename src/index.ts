8893ef73d0e8384bb452227b19139d53c9db2705
Petter Sæther Moen <pettersmoen@gmail.com>
2020-11-26 14:51:04 +0100

    Closing templates

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,12 @@ const parseTree = parser.parse(content);
 
 const ast = toAST(parseTree);
 
+
+function fail(message: string) {
+  console.error(message)
+  process.exit(1)
+}
+
 const sExprTransformer: Transformer<ASTNode, string> = {
   default: (node, children) => `${node.type} (${children.join(", ")})`,
 };
@@ -26,21 +32,66 @@ const sExprs = transform(ast, sExprTransformer);
 const idTransform: Transform = (node, children) => ({ ...node, children });
 
 function getTemplates(program: ASTNode) {
-  const templates: { identifier: string; body: ASTNode[] }[] = [];
+  function isClosed(body: ASTNode[]) {
+    return !body.some(child => child.type === 'inst_statement')
+  }
+  const templates: { identifier: string; body: ASTNode[], closed: boolean }[] = [];
   const findTemplatesTransform: Transformer<ASTNode, ASTNode> = {
     template_declaration: (node, children) => {
       const identifier =
         children.find((child) => child.type === "identifier")?.text || "";
       const body =
         children.find((child) => child.type === "package_template_body")
-          ?.children || [];
-      templates.push({ identifier, body });
+          ?.children.slice(1, -1) || [];
+      templates.push({ identifier, body, closed: isClosed(body) });
       return idTransform(node, children);
     },
     default: idTransform,
   };
-  transform(ast, findTemplatesTransform);
+  transform(program, findTemplatesTransform);
   return templates;
 }
 
-console.log(getTemplates(ast));
+
+const templates = getTemplates(ast);
+
+const inst = transform(ast, {
+  template_declaration: (node, children) => {
+      const identifier =
+        children.find((child) => child.type === "identifier")?.text || "";
+      const body =
+        children.find((child) => child.type === "package_template_body")
+          ?.children || [];
+
+      if(templates.find(el => identifier === el.identifier)?.closed) {
+        return idTransform(node, children)
+      }
+      
+      let res : ASTNode | ASTNode[] = {...node, children}
+      let inst = false;
+      do {
+        inst = false;
+        res = transform(res, {
+          inst_statement: (node, children) => {
+            inst = true
+            const instId = children.find(child => child.type === 'identifier')?.text || ""
+            const template = templates.find(t => t.identifier === instId)
+            if(template === undefined) {
+              fail("Instantiating undefined template, " + instId)
+              return idTransform(node, children) 
+            } else {
+              return template.body
+            }
+          },
+          default: idTransform
+        }) as ASTNode
+      } while(inst)
+
+      return res;
+  },
+  default: idTransform
+}) as ASTNode
+
+
+const newTemplates = getTemplates(inst)
+console.log(newTemplates.map(el => el.body))
--- src/transform.ts
+++ src/transform.ts
@@ -3,11 +3,11 @@ import { ASTNode } from "./AST";
 type BaseInput = { type: string; children: BaseInput[] };
 export type Transform<
   InputNode extends BaseInput = ASTNode,
-  OutputNode = ASTNode
-> = (inp: Omit<InputNode, "children">, children: OutputNode[]) => OutputNode;
+  OutputNode = ASTNode 
+> = (inp: Omit<InputNode, "children">, children: OutputNode[]) => OutputNode | OutputNode[];
 export interface Transformer<
   InputNode extends BaseInput = ASTNode,
-  OutputNode = ASTNode
+  OutputNode = ASTNode 
 > {
   [key: string]: Transform<InputNode, OutputNode>;
   default: Transform<InputNode, OutputNode>;
@@ -20,7 +20,7 @@ export default function transform<
   { children, ...node }: ASTNode,
   transformer: Transformer<InputNode, OutputNode>
 ) {
-  const transformedChildren: OutputNode[] = children.map((child) =>
+  const transformedChildren: OutputNode[] = children.flatMap((child) =>
     transform(child, transformer)
   );
 
