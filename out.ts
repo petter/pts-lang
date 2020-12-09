17ce016f9fc0443502d66ff5d6d455b2b4ac39c7
Petter Sæther Moen <pettersmoen@gmail.com>
2020-12-09 19:22:41 +0100

    Forbedre ts-output litt

--- out.ts
+++ out.ts
@@ -1,1 +1,19 @@
- class B { x = 0 ; } class C extends B { j = 1 ; a = new B ( ) ; constructor ( ) { this . i = 2 ; this . j = 3 ; } } class X { constructor ( ) { let a = new C ( ) this . a = new B ( ) ; this . i = this . a . i ; this . j = a . j ; } }
+
+class B {
+x = 0
+;
+}
+class C extends B {
+j = 1
+;
+a = new B ( )
+;
+constructor ( ) {
+this.i = 2 ;}
+}
+class X {
+constructor ( ) {
+let a = new C ( )
+this.a = new B ( ) ;
+this.i = this.a.i ;}
+}
--- src/transform/toTS.ts
+++ src/transform/toTS.ts
@@ -5,7 +5,12 @@ export default function toTS(program: ASTNode) {
   return transform<ASTNode, string>(program, {
     template_declaration: () => "",
     package_declaration: (_, children) => children.slice(2).join(" "),
-    package_template_body: (_, children) => children.slice(1, -1).join(" "),
+    package_template_body: (_, children) => children.slice(1, -1).join("\n"),
+    statement_block: (_, children) =>
+        `${children[0]}\n${children.slice(1, -2).join('\n')}${children[children.length-1]}`,
+    class_body: (_, children) => children.join("\n"),
+    member_expression: (_, children) => children.join(''),
+    program: (_, children) => children.join('\n'),
     default: (node, children) => {
       if (children.length === 0) {
         return node.text;
