7e5215073b308e44520722af5264fcfa2c2d2205
Petter Sæther Moen <pettersmoen@gmail.com>
2021-02-08 19:57:00 +0100

    Add some utility functions for working with nullable values

--- src/util/index.ts
+++ src/util/index.ts
@@ -9,3 +9,14 @@ export function replace<T>(elements: T[], replaceWith: T, check: (el: T) => bool
 export const typeIs = (type: string) => (objectWithType: { type: string }) => objectWithType.type === type;
 export const identifierIs = (identifier: string) => (objectWithIdentifier: { identifier: string }) =>
     objectWithIdentifier.identifier === identifier;
+
+export function isNotNull<T>(something: T | null): something is T {
+    return something !== null;
+}
+export function filterNull<T>(elements: (T | null)[]): T[] {
+    return elements.filter(isNotNull);
+}
+export function filterMap<T>(elements: T[], func: (el: T) => T | null) {
+    const mappedElements = elements.map(func);
+    return filterNull(mappedElements);
+}
