a7580d4553972a469df5a0a03a554b605f2324db
Petter Sæther Moen <pettersmoen@gmail.com>
2021-02-08 19:58:11 +0100

    Add test for class simple class merging

---
+++ __test__/merge-inst-of-same-template.test.ts
@@ -0,0 +1,26 @@
+import test from 'ava';
+import transpile from '../src';
+
+test('Correctly merges the bodies of the class with the same name and no overlapping members', (t) => {
+    const program = `
+template T {
+    class A {
+        i = 0;
+    }
+}
+
+package P {
+    inst T;
+    inst T { A -> A (i -> j) };
+}
+`;
+
+    const expected = `class A {
+    i = 0;
+    j = 0;
+}
+`;
+    const result = transpile(program, { emitFile: false, targetLanguage: 'ts' });
+
+    t.is(result, expected);
+});
