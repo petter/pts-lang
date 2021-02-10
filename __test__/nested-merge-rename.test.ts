3393d57f57d33a2955fcd68d8df3545b5041c9ce
Petter Sæther Moen <pettersmoen@gmail.com>
2021-02-10 12:03:00 +0100

    Add test for nested merging and renaming

---
+++ __test__/nested-merge-rename.test.ts
@@ -0,0 +1,38 @@
+import test from 'ava';
+import transpile from '../src';
+
+test('Merges class A and B and then C', (t) => {
+    const program = `
+template T1 {
+    class A {
+        i = 0;
+    }
+}
+
+template T2 {
+    class B {
+        i = 0;
+    }
+}
+
+template T3 {
+    inst T1;
+    inst T2 { B -> A (i -> j) };
+}
+
+package P {
+    inst T1;
+    inst T3 { A -> A (i -> k) };
+}
+`;
+
+    const expected = `class A {
+    i = 0;
+    k = 0;
+    j = 0;
+}
+`;
+    const result = transpile(program, { emitFile: false, targetLanguage: 'ts' });
+
+    t.is(result, expected);
+});
